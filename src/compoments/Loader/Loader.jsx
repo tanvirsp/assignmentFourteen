f78a99b46d9b8a9903e2fa2cb03abb259fecf86e
tanvirsp <tanvirgraphic@gmail.com>
2023-11-13 00:32:05 +0600

    Assignment Done

--- src/compoments/Loader/Loader.jsx
+++ src/compoments/Loader/Loader.jsx
@@ -3,9 +3,9 @@ import { Spinner } from 'react-bootstrap';
 
 const Loader = () => {
     return (
-        <>
+        <div className='d-flex justify-content-center align-items-center p-5'>
             <Spinner animation="border" variant="success" />
-        </>
+        </div>
     );
 };
 
